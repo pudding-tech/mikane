65fc0c4f3e0f2bc62d6c3eb9f7bdf5417a9420f0
Adrian Hundseth <adrian.hundseth@gmail.com>
2022-08-03 19:54:41 +0200

    show even name in title

--- app/pudding-debt/src/app/pages/events/event/event.component.ts
+++ app/pudding-debt/src/app/pages/events/event/event.component.ts
@@ -1,15 +1,21 @@
-import { Component } from '@angular/core';
-import { Router } from '@angular/router';
-import { PuddingEvent } from 'src/app/services/event/event.service';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { map, combineLatest } from 'rxjs';
+import {
+	EventService,
+	PuddingEvent,
+} from 'src/app/services/event/event.service';
 
 @Component({
 	selector: 'app-event',
 	templateUrl: './event.component.html',
 	styleUrls: ['./event.component.scss'],
 })
-export class EventComponent {
-	event: PuddingEvent;
-  activeLink = '';
+export class EventComponent implements OnInit {
+	event: PuddingEvent = {
+        name: 'PUDDING DEBT',
+    } as PuddingEvent;
+	activeLink = '';
 	links = [
 		{
 			name: 'Participants',
@@ -29,9 +35,22 @@ export class EventComponent {
 		},
 	];
 
-	constructor(private router: Router) {
-		this.event =
-			this.router.getCurrentNavigation()?.extras.state?.['event'] ?? {};
-		console.log('event', this.event);
+	constructor(
+		private eventService: EventService,
+		private route: ActivatedRoute
+	) {}
+
+	ngOnInit() {
+		combineLatest([this.eventService.loadEvents(), this.route.params]).pipe(
+			map(([events, params]) => {
+				return events.find((event) => {
+					return event.id === +params['eventId'];
+				});
+			})
+		).subscribe(event => {
+            if (event) {
+                this.event = event;
+            }
+        });
 	}
 }
--- app/pudding-debt/src/app/pages/payment-structure/payment-structure.component.ts
+++ app/pudding-debt/src/app/pages/payment-structure/payment-structure.component.ts
@@ -18,6 +18,8 @@ export class PaymentStructureComponent implements OnInit {
     
 	private eventId!: number;
 
+    displayedColumns: string[] = ['name', 'amount'];
+
 	payments: Payment[] = [];
 	senders: {
 		sender: User;
