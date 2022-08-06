288639599fff3ae8b8aa9a18badf19aae0f00b78
MotionDuck <andreasoeium@gmail.com>
2022-08-06 14:55:35 +0200

    Fix order in db_init script

--- server/db_scripts/db_init.sh
+++ server/db_scripts/db_init.sh
@@ -1,15 +1,12 @@
 # Wait to be sure that SQL Server came up
-sleep 30s
+sleep 10s
 
 echo "Running db_init script"
-echo ${DB_SERVER}
-echo ${DB_DATABASE}
 
 # Run the setup script to create the DB and the schema in the DB
 # Note: make sure that your password matches what is in the Dockerfile
 /opt/mssql-tools/bin/sqlcmd -S ${DB_SERVER} -U sa -P ${DB_PASSWORD} -d master -i db_scripts/db_init.sql
 
-/opt/mssql-tools/bin/sqlcmd -S ${DB_SERVER} -U sa -P ${DB_PASSWORD} -d puddingdebt -i db_scripts/db_test_data.sql
 /opt/mssql-tools/bin/sqlcmd -S ${DB_SERVER} -U sa -P ${DB_PASSWORD} -d puddingdebt -i db_scripts/add_user_to_category.sql
 /opt/mssql-tools/bin/sqlcmd -S ${DB_SERVER} -U sa -P ${DB_PASSWORD} -d puddingdebt -i db_scripts/delete_category.sql
 /opt/mssql-tools/bin/sqlcmd -S ${DB_SERVER} -U sa -P ${DB_PASSWORD} -d puddingdebt -i db_scripts/delete_event.sql
@@ -27,3 +24,4 @@ echo ${DB_DATABASE}
 /opt/mssql-tools/bin/sqlcmd -S ${DB_SERVER} -U sa -P ${DB_PASSWORD} -d puddingdebt -i db_scripts/new_expense.sql
 /opt/mssql-tools/bin/sqlcmd -S ${DB_SERVER} -U sa -P ${DB_PASSWORD} -d puddingdebt -i db_scripts/new_user.sql
 /opt/mssql-tools/bin/sqlcmd -S ${DB_SERVER} -U sa -P ${DB_PASSWORD} -d puddingdebt -i db_scripts/remove_user_from_category.sql
+/opt/mssql-tools/bin/sqlcmd -S ${DB_SERVER} -U sa -P ${DB_PASSWORD} -d puddingdebt -i db_scripts/db_test_data.sql
