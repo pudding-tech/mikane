dadbd84825cb0d988ae9f0441a1c3b32e567d46a
MotionDuck <andreasoeium@gmail.com>
2023-03-16 02:28:47 +0100

    Move dropdown from menu to split-button

--- app/pudding-debt/src/app/features/menu/menu.component.ts
+++ app/pudding-debt/src/app/features/menu/menu.component.ts
@@ -1,7 +1,6 @@
 import { Component, ElementRef, ViewChild } from '@angular/core';
 import { NgIf } from '@angular/common';
 import { Router } from '@angular/router';
-import { animate, style, transition, trigger } from '@angular/animations';
 import { MatIconModule } from '@angular/material/icon';
 import { SplitButtonComponent } from 'src/app/features/split-button/split-button.component';
 import { ApiError } from 'src/app/types/apiError.type';
@@ -13,24 +12,10 @@ import { AuthService } from 'src/app/services/auth/auth.service';
 	templateUrl: './menu.component.html',
 	styleUrls: ['./menu.component.scss'],
 	standalone: true,
-	animations: [
-		trigger('overlayAnimation', [
-			transition(':enter', [style({ opacity: 0, transform: 'scaleY(0.8)' }), animate('{{showTransitionParams}}')]),
-			transition(':leave', [animate('{{hideTransitionParams}}', style({ opacity: 0 }))])
-		])
-	],
-	host: {
-		'(document:click)': 'onOutsideClick($event)'
-	},
 	imports: [NgIf, MatIconModule, SplitButtonComponent],
 })
 export class MenuComponent {
-	@ViewChild('dropdown') private dropdown: ElementRef;
-	@ViewChild('splitButton', { read: ElementRef }) private splitButton: ElementRef;
-
-	toggled = false;
-	showTransitionOptions = '.12s cubic-bezier(0, 0, 0.2, 1)';
-	hideTransitionOptions = '.1s linear';
+	@ViewChild('splitButton') private splitButton: SplitButtonComponent;
 
 	constructor(
 		private router: Router,
@@ -38,28 +23,24 @@ export class MenuComponent {
 		private authService: AuthService
 	) {}
 
+	onDropdownClick = (index: number) => {
+		if (index === 1) {
+			this.onAccountClick();
+		}
+		else if (index === 2) {
+			this.logout();
+		}
+	}
+
 	onAccountClick = () => {
 		if (this.router.url === '/settings') {
-			this.toggled = false;
+			this.splitButton.toggled = false;
 			return;
 		}
 		this.router.navigate(['/settings']);
 	};
 
-	toggleDropdown = () => {
-		this.toggled = !this.toggled;
-	};
-
-	onOutsideClick(e: any) {
-		if (!this.toggled) {
-			return;
-		}
-		if (this.dropdown && !this.dropdown.nativeElement.contains(e.target) && !this.splitButton.nativeElement.contains(e.target)) {
-			this.toggled = !this.toggled;
-		}
-	}
-
-	logout() {
+	logout = () => {
 		this.authService.logout().subscribe({
 			next: () => {
 				this.router.navigate(['/login']);
--- app/pudding-debt/src/app/features/split-button/split-button.component.ts
+++ app/pudding-debt/src/app/features/split-button/split-button.component.ts
@@ -1,4 +1,6 @@
-import { Component, Input } from '@angular/core';
+import { Component, ElementRef, EventEmitter, Input, Output } from '@angular/core';
+import { NgIf } from '@angular/common';
+import { animate, style, transition, trigger } from '@angular/animations';
 import { MatButtonToggleModule } from '@angular/material/button-toggle';
 import { MatIconModule } from '@angular/material/icon';
 
@@ -7,9 +9,44 @@ import { MatIconModule } from '@angular/material/icon';
 	templateUrl: './split-button.component.html',
 	styleUrls: ['./split-button.component.scss'],
 	standalone: true,
-	imports: [MatButtonToggleModule, MatIconModule],
+	animations: [
+		trigger('overlayAnimation', [
+			transition(':enter', [style({ opacity: 0, transform: 'scaleY(0.8)' }), animate('{{showTransitionParams}}')]),
+			transition(':leave', [animate('{{hideTransitionParams}}', style({ opacity: 0 }))])
+		])
+	],
+	host: {
+		'(document:click)': 'onOutsideClick($event)'
+	},
+	imports: [NgIf, MatButtonToggleModule, MatIconModule],
 })
 export class SplitButtonComponent {
-	@Input() onClick: ($event: MouseEvent) => void;
-	@Input() toggleDropdown: ($event: MouseEvent) => void;
+	@Input() onClick: () => void;
+	@Output() onDropdownClick: EventEmitter<any> = new EventEmitter();
+
+	self: ElementRef;
+	toggled = false;
+	showTransitionOptions = '.12s cubic-bezier(0, 0, 0.2, 1)';
+	hideTransitionOptions = '.1s linear';
+
+	constructor (element: ElementRef) {
+		this.self = element;
+	};
+
+	toggleDropdown = () => {
+		this.toggled = !this.toggled;
+	};
+
+	dropdownClicked = (index: number) => {
+		this.onDropdownClick.emit(index);
+	}
+
+	onOutsideClick(event: any) {
+		if (!this.toggled) {
+			return;
+		}
+		if (!this.self.nativeElement.contains(event.target)) {
+			this.toggled = !this.toggled;
+		}
+	}
 }
