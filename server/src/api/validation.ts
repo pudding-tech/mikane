f1d79b59965230e576c469f360e4fec13f3a8fe2
MotionDuck <andreasoeium@gmail.com>
2023-07-27 03:01:43 +0200

    Allow validation endpoints to be used when editing

--- server/src/api/validation.ts
+++ server/src/api/validation.ts
@@ -14,14 +14,18 @@ const router = express.Router();
 router.post("/validation/user/username", async (req, res, next) => {
   try {
     const username: string = req.body.username;
+    const userId: string | undefined = req.body.userId;
     if (!username) {
       throw new ErrorExt(ec.PUD109);
     }
     if (username.trim() === "") {
       throw new ErrorExt(ec.PUD059);
     }
+    if (userId !== undefined && !isUUID(userId)) {
+      throw new ErrorExt(ec.PUD016);
+    }
 
-    const valid = await db.validateUsername(username.trim());
+    const valid = await db.validateUsername(username.trim(), userId);
     if (!valid) {
       throw new ErrorExt(ec.PUD017);
     }
@@ -38,14 +42,18 @@ router.post("/validation/user/username", async (req, res, next) => {
 router.post("/validation/user/email", async (req, res, next) => {
   try {
     const email: string = req.body.email;
+    const userId: string | undefined = req.body.userId;
     if (!email) {
       throw new ErrorExt(ec.PUD110);
     }
     if (!isEmail(email)) {
       throw new ErrorExt(ec.PUD004);
     }
+    if (userId !== undefined && !isUUID(userId)) {
+      throw new ErrorExt(ec.PUD016);
+    }
 
-    const valid = await db.validateEmail(email.trim());
+    const valid = await db.validateEmail(email.trim(), userId);
     if (!valid) {
       throw new ErrorExt(ec.PUD018);
     }
@@ -62,14 +70,18 @@ router.post("/validation/user/email", async (req, res, next) => {
 router.post("/validation/user/phone", async (req, res, next) => {
   try {
     const phoneNumber: string = req.body.phone;
+    const userId: string | undefined = req.body.userId;
     if (!phoneNumber) {
       throw new ErrorExt(ec.PUD111);
     }
     if (!isPhoneNumber(phoneNumber)) {
       throw new ErrorExt(ec.PUD113);
     }
+    if (userId !== undefined && !isUUID(userId)) {
+      throw new ErrorExt(ec.PUD016);
+    }
 
-    const valid = await db.validatePhoneNumber(phoneNumber.trim());
+    const valid = await db.validatePhoneNumber(phoneNumber.trim(), userId);
     if (!valid) {
       throw new ErrorExt(ec.PUD019);
     }
@@ -86,14 +98,18 @@ router.post("/validation/user/phone", async (req, res, next) => {
 router.post("/validation/event/name", authCheck, async (req, res, next) => {
   try {
     const name: string = req.body.name;
+    const eventId: string | undefined = req.body.eventId;
     if (!name) {
       throw new ErrorExt(ec.PUD112);
     }
     if (name.trim() === "") {
       throw new ErrorExt(ec.PUD059);
     }
+    if (eventId !== undefined && !isUUID(eventId)) {
+      throw new ErrorExt(ec.PUD013);
+    }
 
-    const valid = await db.validateEventName(name.trim());
+    const valid = await db.validateEventName(name.trim(), eventId);
     if (!valid) {
       throw new ErrorExt(ec.PUD005);
     }
@@ -111,6 +127,7 @@ router.post("/validation/category/name", authCheck, async (req, res, next) => {
   try {
     const name: string = req.body.name;
     const eventId: string = req.body.eventId;
+    const categoryId: string | undefined = req.body.categoryId;
     if (!name) {
       throw new ErrorExt(ec.PUD112);
     }
@@ -120,8 +137,11 @@ router.post("/validation/category/name", authCheck, async (req, res, next) => {
     if (!isUUID(eventId)) {
       throw new ErrorExt(ec.PUD013);
     }
+    if (categoryId !== undefined && !isUUID(categoryId)) {
+      throw new ErrorExt(ec.PUD045);
+    }
 
-    const valid = await db.validateCategoryName(name.trim(), eventId);
+    const valid = await db.validateCategoryName(name.trim(), eventId, categoryId);
     if (!valid) {
       throw new ErrorExt(ec.PUD097);
     }
--- server/src/db/dbValidation.ts
+++ server/src/db/dbValidation.ts
@@ -9,14 +9,15 @@ import { PUD006 } from "../types/errorCodes";
 /**
  * DB interface: Check if a username is available
  * @param username
+ * @param userId Provide user ID if checking against a specific user
  * @returns True if username is available
  */
-export const validateUsername = async (username: string) => {
+export const validateUsername = async (username: string, userId?: string) => {
   const query = {
     text: `
-      SELECT false AS available FROM "user" u WHERE u.username ILIKE $1;
+      SELECT false AS available FROM "user" u WHERE u.username ILIKE $1 AND ($2::uuid is null OR u.id != $2::uuid);
     `,
-    values: [username]
+    values: [username, userId]
   };
   const data = await pool.query(query);
   if (data.rows[0] && data.rows[0].available === false) {
@@ -28,14 +29,15 @@ export const validateUsername = async (username: string) => {
 /**
  * DB interface: Check if a user email is available
  * @param email
+ * @param userId Provide user ID if checking against a specific user
  * @returns True if email is available
  */
-export const validateEmail = async (email: string) => {
+export const validateEmail = async (email: string, userId?: string) => {
   const query = {
     text: `
-      SELECT false AS available FROM "user" u WHERE u.email ILIKE $1;
+      SELECT false AS available FROM "user" u WHERE u.email ILIKE $1 AND ($2::uuid is null OR u.id != $2::uuid);
     `,
-    values: [email]
+    values: [email, userId]
   };
   const data = await pool.query(query);
   if (data.rows[0] && data.rows[0].available === false) {
@@ -47,14 +49,15 @@ export const validateEmail = async (email: string) => {
 /**
  * DB interface: Check if a user phone number is available
  * @param phoneNumber
+ * @param userId Provide user ID if checking against a specific user
  * @returns True if phone number is available
  */
-export const validatePhoneNumber = async (phoneNumber: string) => {
+export const validatePhoneNumber = async (phoneNumber: string, userId?: string) => {
   const query = {
     text: `
-      SELECT false AS available FROM "user" u WHERE u.phone_number ILIKE $1;
+      SELECT false AS available FROM "user" u WHERE u.phone_number ILIKE $1 AND ($2::uuid is null OR u.id != $2::uuid);
     `,
-    values: [phoneNumber]
+    values: [phoneNumber, userId]
   };
   const data = await pool.query(query);
   if (data.rows[0] && data.rows[0].available === false) {
@@ -70,14 +73,15 @@ export const validatePhoneNumber = async (phoneNumber: string) => {
 /**
  * DB interface: Check if a event name is available
  * @param name
+ * @param userId Provide event ID if checking against a specific event
  * @returns True if event name is available
  */
-export const validateEventName = async (name: string) => {
+export const validateEventName = async (name: string, eventId?: string) => {
   const query = {
     text: `
-      SELECT false AS available FROM "event" e WHERE e.name ILIKE $1;
+      SELECT false AS available FROM "event" e WHERE e.name ILIKE $1 AND ($2::uuid is null OR e.id != $2::uuid);
     `,
-    values: [name]
+    values: [name, eventId]
   };
   const data = await pool.query(query);
   if (data.rows[0] && data.rows[0].available === false) {
@@ -94,9 +98,10 @@ export const validateEventName = async (name: string) => {
  * DB interface: Check if a category name is available within an event
  * @param name
  * @param eventId
+ * @param userId Provide category ID if checking against a specific category
  * @returns True if category name is available
  */
-export const validateCategoryName = async (name: string, eventId: string) => {
+export const validateCategoryName = async (name: string, eventId: string, categoryId?: string) => {
   const queryEventCheck = {
     text: `
       SELECT true AS exists FROM "event" e WHERE e.id = $1;
@@ -112,9 +117,9 @@ export const validateCategoryName = async (name: string, eventId: string) => {
 
   const query = {
     text: `
-      SELECT false AS available FROM category c WHERE c.name ILIKE $1 AND c.event_id = $2;
+      SELECT false AS available FROM category c WHERE c.name ILIKE $1 AND c.event_id = $2 AND ($3::uuid is null OR c.id != $3::uuid);
     `,
-    values: [name, eventId]
+    values: [name, eventId, categoryId]
   };
   const data = await pool.query(query);
   if (data.rows[0] && data.rows[0].available === false) {
