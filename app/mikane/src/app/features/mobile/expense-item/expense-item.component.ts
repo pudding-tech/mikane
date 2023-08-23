aa5ae544d6e96215cbfd9f2bb5b9f85404725f95
MotionDuck <andreasoeium@gmail.com>
2023-08-23 21:19:44 +0200

    Add expense page where users can edit/delete expense

--- app/mikane/src/app/features/mobile/expense-item/expense-item.component.ts
+++ app/mikane/src/app/features/mobile/expense-item/expense-item.component.ts
@@ -1,6 +1,7 @@
 import { Component, Input } from '@angular/core';
 import { MatIconModule } from '@angular/material/icon';
 import { CurrencyPipe } from '@angular/common';
+import { ActivatedRoute, Router } from '@angular/router';
 import { Expense } from 'src/app/services/expense/expense.service';
 import { MatListModule } from '@angular/material/list';
 
@@ -13,4 +14,10 @@ import { MatListModule } from '@angular/material/list';
 })
 export class ExpenseItemComponent {
 	@Input('expense') expense: Expense;
+
+	constructor(private router: Router, private route: ActivatedRoute) {};
+
+	gotoExpense() {
+		this.router.navigate([this.expense.id], { relativeTo: this.route });
+	}
 }
--- app/mikane/src/app/pages/expenditures/expenditures.routes.ts
+++ app/mikane/src/app/pages/expenditures/expenditures.routes.ts
@@ -1,4 +1,8 @@
 import { Route } from '@angular/router';
 import { ExpendituresComponent } from './expenditures.component';
+import { ExpenseComponent } from './expense/expense.component';
 
-export default [{ path: '', component: ExpendituresComponent }] as Route[];
+export default [
+  { path: '', component: ExpendituresComponent },
+  { path: ':id', component: ExpenseComponent},
+] as Route[];
