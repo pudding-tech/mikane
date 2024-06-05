8f6026fd133d2553abd2cf44dbe627e0dcb5c5ee
Adrian H <adrian.hundseth@gmail.com>
2024-06-05 03:14:00 +0200

    feat: use mat slide toggle for user preferences

--- app/mikane/src/app/pages/account/preferences/preferences.component.spec.ts
+++ app/mikane/src/app/pages/account/preferences/preferences.component.spec.ts
@@ -1,62 +1,99 @@
-import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
+import { HarnessLoader } from '@angular/cdk/testing';
+import { TestbedHarnessEnvironment } from '@angular/cdk/testing/testbed';
 import { CommonModule } from '@angular/common';
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatIconModule } from '@angular/material/icon';
-import { MatInputModule } from '@angular/material/input';
-import { MatButtonToggleModule } from '@angular/material/button-toggle';
-import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatSlideToggleModule } from '@angular/material/slide-toggle';
+import { MatSlideToggleHarness } from '@angular/material/slide-toggle/testing';
+import { MockModule } from 'ng-mocks';
+import { of, throwError } from 'rxjs';
 import { MessageService } from 'src/app/services/message/message.service';
-import { UserService } from 'src/app/services/user/user.service';
-import { User } from 'src/app/services/user/user.service';
+import { User, UserService } from 'src/app/services/user/user.service';
 import { PreferencesComponent } from './preferences.component';
 
 describe('PreferencesComponent', () => {
-  let component: PreferencesComponent;
-  let fixture: ComponentFixture<PreferencesComponent>;
+	let component: PreferencesComponent;
+	let fixture: ComponentFixture<PreferencesComponent>;
+	let loader: HarnessLoader;
+	let userServiceSpy: jasmine.SpyObj<UserService>;
+	let messageServiceSpy: jasmine.SpyObj<MessageService>;
 
-  beforeEach(waitForAsync(() => {
-    TestBed.configureTestingModule({
-      imports: [
-        CommonModule,
-        MatCardModule,
-        MatIconModule,
-        FormsModule,
-        ReactiveFormsModule,
-        MatFormFieldModule,
-        MatInputModule,
-        MatButtonModule,
-        MatButtonToggleModule,
-        MatProgressSpinnerModule,
-        BrowserAnimationsModule,
-      ],
-      providers: [
-        { provide: UserService, useValue: {} },
-        { provide: MessageService, useValue: {} },
-      ],
-    }).compileComponents();
-  }));
+	beforeEach(waitForAsync(() => {
+		userServiceSpy = jasmine.createSpyObj('UserService', ['editUserPreferences']);
+		messageServiceSpy = jasmine.createSpyObj('MessageService', ['showError']);
+		TestBed.configureTestingModule({
+			imports: [
+				CommonModule,
+				PreferencesComponent,
+				MockModule(MatCardModule),
+				MockModule(MatIconModule),
+				MockModule(MatFormFieldModule),
+				FormsModule,
+				ReactiveFormsModule,
+				MatSlideToggleModule,
+			],
+			providers: [
+				{ provide: UserService, useValue: userServiceSpy },
+				{ provide: MessageService, useValue: messageServiceSpy },
+			],
+		}).compileComponents();
+	}));
 
-  beforeEach(() => {
-    fixture = TestBed.createComponent(PreferencesComponent);
-    component = fixture.componentInstance;
-    component.user = {
-      id: '1',
-      publicEmail: false,
-      publicPhone: true,
-    } as User;
-    fixture.detectChanges();
-  });
+	beforeEach(() => {
+		fixture = TestBed.createComponent(PreferencesComponent);
+		component = fixture.componentInstance;
+		loader = TestbedHarnessEnvironment.loader(fixture);
+		component.user = {
+			id: '1',
+			publicEmail: false,
+			publicPhone: true,
+		} as User;
+		fixture.detectChanges();
+	});
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
+	it('should create', () => {
+		expect(component).toBeTruthy();
+	});
 
-  it('should initialize publicEmail and publicPhone from user', () => {
-    expect(component.user.publicEmail).toEqual(false);
-    expect(component.user.publicPhone).toEqual(true);
-  });
+	it('should initialize publicEmail and publicPhone from user', () => {
+		expect(component.user.publicEmail).toEqual(false);
+		expect(component.user.publicPhone).toEqual(true);
+	});
+
+	it('should toggle email setting', async () => {
+		userServiceSpy.editUserPreferences.and.returnValue(of(component.user));
+		const toggles = await loader.getAllHarnesses(MatSlideToggleHarness);
+		await toggles[0].toggle();
+		fixture.detectChanges();
+
+		expect(userServiceSpy.editUserPreferences).toHaveBeenCalledWith('1', true, undefined);
+	});
+
+	it('should toggle phone setting', async () => {
+		userServiceSpy.editUserPreferences.and.returnValue(of(component.user));
+		const toggles = await loader.getAllHarnesses(MatSlideToggleHarness);
+		await toggles[1].toggle();
+		fixture.detectChanges();
+
+		expect(userServiceSpy.editUserPreferences).toHaveBeenCalledWith('1', undefined, false);
+	});
+
+	it('should handle error', async () => {
+		userServiceSpy.editUserPreferences.and.returnValue(throwError(() => new Error('Error')));
+		const toggles = await loader.getAllHarnesses(MatSlideToggleHarness);
+		await toggles[0].toggle();
+		fixture.detectChanges();
+
+		expect(messageServiceSpy.showError).toHaveBeenCalledWith('Failed to change user preferences');
+
+		messageServiceSpy.showError.calls.reset();
+
+		await toggles[1].toggle();
+		fixture.detectChanges();
+
+		expect(messageServiceSpy.showError).toHaveBeenCalledWith('Failed to change user preferences');
+	});
 });
--- app/mikane/src/app/pages/account/preferences/preferences.component.ts
+++ app/mikane/src/app/pages/account/preferences/preferences.component.ts
@@ -1,53 +1,26 @@
 import { CommonModule } from '@angular/common';
-import { Component, Input, OnDestroy } from '@angular/core';
+import { Component, Input, OnDestroy, OnInit } from '@angular/core';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatIconModule } from '@angular/material/icon';
-import { MatInputModule } from '@angular/material/input';
-import { MatButtonToggleModule } from '@angular/material/button-toggle';
-import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
+import { MatSlideToggleModule } from '@angular/material/slide-toggle';
 import { Subscription } from 'rxjs';
 import { BreakpointService } from 'src/app/services/breakpoint/breakpoint.service';
 import { MessageService } from 'src/app/services/message/message.service';
 import { User, UserService } from 'src/app/services/user/user.service';
 import { ApiError } from 'src/app/types/apiError.type';
-import { animate, state, style, transition, trigger } from '@angular/animations';
 
 @Component({
 	selector: 'app-preferences',
 	templateUrl: './preferences.component.html',
 	styleUrls: ['./preferences.component.scss'],
 	standalone: true,
-	imports: [
-		CommonModule,
-		MatCardModule,
-		MatIconModule,
-		FormsModule,
-		ReactiveFormsModule,
-		MatFormFieldModule,
-		MatInputModule,
-		MatButtonModule,
-		MatButtonToggleModule,
-		MatProgressSpinnerModule,
-	],
-	animations: [
-    trigger('fadeInOutAnimation', [
-      state('in', style({ opacity: 1 })),
-      transition('void => *', [
-        style({ opacity: 0 }),
-        animate(200, style({ opacity: 1 }))
-      ]),
-      transition('* => void', [
-        animate(200, style({ opacity: 0 }))
-      ])
-    ])
-  ]
+	imports: [CommonModule, MatCardModule, MatIconModule, FormsModule, ReactiveFormsModule, MatFormFieldModule, MatSlideToggleModule],
 })
-export class PreferencesComponent implements OnDestroy {
+export class PreferencesComponent implements OnInit, OnDestroy {
 	@Input() user: User;
-	private editPreferencesSubscription: Subscription;
+	private editPreferencesSubscription = new Subscription();
 	protected publicEmail: boolean;
 	protected publicPhone: boolean;
 	protected loadingEmail: boolean;
@@ -58,7 +31,7 @@ export class PreferencesComponent implements OnDestroy {
 	constructor(
 		private userService: UserService,
 		private messageService: MessageService,
-		public breakpointService: BreakpointService
+		public breakpointService: BreakpointService,
 	) {}
 
 	ngOnInit(): void {
@@ -66,44 +39,40 @@ export class PreferencesComponent implements OnDestroy {
 		this.publicPhone = this.user.publicPhone;
 	}
 
-	editUserPreferences(type: 'email' | 'phone') {
-		if (type === 'email') {
-			this.loadingEmail = true;
-		}
-		else if (type === 'phone') {
-			this.loadingPhone = true;
-		}
-		this.editPreferencesSubscription = this.userService
-			.editUserPreferences(
-				this.user.id,
-				type === 'email' ? this.publicEmail : undefined,
-				type === 'phone' ? this.publicPhone : undefined
-			)
-			.subscribe({
+	toggleEmail() {
+		this.loadingEmail = true;
+		this.editPreferencesSubscription.add(
+			this.userService.editUserPreferences(this.user.id, this.publicEmail, undefined).subscribe({
 				next: (user) => {
 					this.user = user;
-					if (type === 'email') {
-						this.loadingEmail = false;
-						this.updatedEmail = true;
-						setTimeout(() => {
-							this.updatedEmail = false;
-						}, 1000);
-					}
-					else if (type === 'phone') {
-						this.loadingPhone = false;
-						this.updatedPhone = true;
-						setTimeout(() => {
-							this.updatedPhone = false;
-						}, 1000);
-					}
+					this.loadingEmail = false;
 				},
 				error: (err: ApiError) => {
+					this.publicEmail = !this.publicEmail;
 					this.messageService.showError('Failed to change user preferences');
-					console.error('something went wrong while changing user preference', err?.error?.message);
+					console.error('something went wrong while changing user email preference', err?.error?.message);
 					this.loadingEmail = false;
+				},
+			}),
+		);
+	}
+
+	togglePhone() {
+		this.loadingPhone = true;
+		this.editPreferencesSubscription.add(
+			this.userService.editUserPreferences(this.user.id, undefined, this.publicPhone).subscribe({
+				next: (user) => {
+					this.user = user;
 					this.loadingPhone = false;
 				},
-			});
+				error: (err: ApiError) => {
+					this.publicPhone = !this.publicPhone;
+					this.messageService.showError('Failed to change user preferences');
+					console.error('something went wrong while changing user phone preference', err?.error?.message);
+					this.loadingPhone = false;
+				},
+			}),
+		);
 	}
 
 	ngOnDestroy(): void {
