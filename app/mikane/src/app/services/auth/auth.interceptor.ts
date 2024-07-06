de833d972dfb0e4cfa1cba56f35febc431283954
Adrian H <adrian.hundseth@gmail.com>
2024-07-06 04:56:36 +0200

    feat: navigate to event list if trying to access private event
    Closes #374

--- app/mikane/src/app/services/auth/auth.interceptor.ts
+++ app/mikane/src/app/services/auth/auth.interceptor.ts
@@ -2,11 +2,16 @@ import { HttpEvent, HttpHandler, HttpInterceptor, HttpRequest } from '@angular/c
 import { Injectable } from '@angular/core';
 import { Router } from '@angular/router';
 import { NEVER, Observable, catchError, throwError } from 'rxjs';
+import { MessageService } from '../message/message.service';
 import { AuthService } from './auth.service';
 
 @Injectable()
 export class AuthInterceptor implements HttpInterceptor {
-	constructor(private router: Router, private authService: AuthService) {}
+	constructor(
+		private router: Router,
+		private authService: AuthService,
+		private messageService: MessageService,
+	) {}
 
 	intercept(req: HttpRequest<unknown>, next: HttpHandler): Observable<HttpEvent<unknown>> {
 		req = req.clone({
@@ -24,10 +29,15 @@ export class AuthInterceptor implements HttpInterceptor {
 					this.authService.clearCurrentUser();
 					this.router.navigate(['/login']);
 					return NEVER;
+				} else if (error?.status === 403 && error?.error?.code === 'PUD-138') {
+					// User is trying to access a private event without authorization, redirect to events page
+					this.messageService.showError('You are not authorized to view this private event');
+					this.router.navigate(['/events']);
+					return NEVER;
 				} else {
 					return throwError(() => error);
 				}
-			})
+			}),
 		);
 	}
 }
