6bd2715efc12d993039460912530652e3528e041
Adrian H <adrian.hundseth@gmail.com>
2025-08-24 05:35:06 +0200

    test: migrate breakpoint test

---
+++ app/mikane/src/app/services/breakpoint/breakpoint.spec.ts
@@ -0,0 +1,68 @@
+import { BreakpointObserver, Breakpoints, BreakpointState } from '@angular/cdk/layout';
+import { TestBed } from '@angular/core/testing';
+import { of } from 'rxjs';
+import { throwError } from 'rxjs/internal/observable/throwError';
+import { vi } from 'vitest';
+import { BreakpointService } from './breakpoint.service';
+
+describe('BreakpointService', () => {
+	let service: BreakpointService;
+	let breakpointObserverMock: BreakpointObserver;
+
+	beforeEach(() => {
+		TestBed.configureTestingModule({
+			providers: [
+				BreakpointService,
+				{
+					provide: BreakpointObserver,
+					useValue: {
+						observe: vi.fn(),
+					},
+				},
+			],
+		});
+
+		service = TestBed.inject(BreakpointService);
+		breakpointObserverMock = TestBed.inject(BreakpointObserver);
+	});
+
+	it('#isMobile should return true if screne size matches HandsetPortrait breakpoint', () => {
+		vi.spyOn(breakpointObserverMock, 'observe').mockImplementation((breakpoint: string | readonly string[]) => {
+			if (breakpoint === Breakpoints.HandsetPortrait) {
+				return of({ matches: true } as BreakpointState);
+			} else {
+				return throwError(() => new Error('wrong breakpoint'));
+			}
+		});
+
+		let result: boolean;
+
+		service.isMobile().subscribe({
+			next: (match: boolean) => {
+				result = match;
+			},
+		});
+
+		expect(result).toBe(true);
+	});
+
+	it('#isMobile should return false if screen size does not match HandsetPortrait breakpoint', () => {
+		vi.spyOn(breakpointObserverMock, 'observe').mockImplementation((breakpoint: string | readonly string[]) => {
+			if (breakpoint === Breakpoints.HandsetPortrait) {
+				return of({ matches: false } as BreakpointState);
+			} else {
+				return throwError(() => new Error('wrong breakpoint'));
+			}
+		});
+
+		let result: boolean;
+
+		service.isMobile().subscribe({
+			next: (match: boolean) => {
+				result = match;
+			},
+		});
+
+		expect(result).toBe(false);
+	});
+});
