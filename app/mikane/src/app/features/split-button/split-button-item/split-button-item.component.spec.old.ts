3c5e76dc9faad7aff740a289a9fb23e61aa10389
Andreas <andreasoeium@gmail.com>
2025-08-25 01:43:45 +0200

    test: migrate tests for split-button, and partly split-button-item. add new tests for split-button-item-directive

--- app/mikane/src/app/features/split-button/split-button-item/split-button-item.component.spec.old.ts
+++ app/mikane/src/app/features/split-button/split-button-item/split-button-item.component.spec.old.ts
@@ -2,7 +2,7 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { MatRippleModule } from '@angular/material/core';
 import { MatIconModule } from '@angular/material/icon';
 import { By } from '@angular/platform-browser';
-import { MockModule } from 'ng-mocks';
+import { describe, it, beforeEach, expect, vi } from 'vitest';
 import { SplitButtonItemComponent } from './split-button-item.component';
 
 describe('SplitButtonItemComponent', () => {
@@ -11,7 +11,7 @@ describe('SplitButtonItemComponent', () => {
 
 	beforeEach(() => {
 		TestBed.configureTestingModule({
-			imports: [SplitButtonItemComponent, MockModule(MatIconModule), MockModule(MatRippleModule)],
+			imports: [SplitButtonItemComponent, MatIconModule, MatRippleModule],
 		}).compileComponents();
 	});
 
@@ -28,19 +28,19 @@ describe('SplitButtonItemComponent', () => {
 	});
 
 	it('should emit the onClick event when the button is clicked', () => {
-		spyOn(component.splitButtonClick, 'emit');
+		const spy = vi.spyOn(component.splitButtonClick, 'emit');
 		const buttonEl = fixture.debugElement.query(By.css('a')).nativeElement as HTMLButtonElement;
 		buttonEl.click();
 
-		expect(component.splitButtonClick.emit).toHaveBeenCalledWith();
+		expect(spy).toHaveBeenCalledWith();
 	});
 
 	it('should emit the onClick event when the button is pressed', () => {
-		spyOn(component.splitButtonClick, 'emit');
+		const spy = vi.spyOn(component.splitButtonClick, 'emit');
 		const buttonEl = fixture.debugElement.query(By.css('a')).nativeElement as HTMLButtonElement;
 		buttonEl.dispatchEvent(new KeyboardEvent('keyup', { key: 'Enter' }));
 
-		expect(component.splitButtonClick.emit).toHaveBeenCalledWith();
+		expect(spy).toHaveBeenCalledWith();
 	});
 
 	it('should display the correct icon', () => {
