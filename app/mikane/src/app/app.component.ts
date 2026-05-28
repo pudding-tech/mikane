9ccb174bcefa95f567bfaf8064d82d1148f0ffc3
Adrian H <adrian.hundseth@gmail.com>
2026-05-28 22:30:56 +0200

    feat: add service worker update handling with snackbar notification

--- app/mikane/src/app/app.component.ts
+++ app/mikane/src/app/app.component.ts
@@ -1,7 +1,10 @@
 import { Component, inject } from '@angular/core';
 import { MatIconRegistry } from '@angular/material/icon';
+import { MatSnackBar } from '@angular/material/snack-bar';
 import { DomSanitizer } from '@angular/platform-browser';
 import { RouterOutlet } from '@angular/router';
+import { SwUpdate, VersionReadyEvent } from '@angular/service-worker';
+import { filter } from 'rxjs';
 import { ENV } from 'src/environments/environment.provider';
 import { FooterComponent } from './features/footer/footer.component';
 import { LogService } from './services/log/log.service';
@@ -17,6 +20,8 @@ export class AppComponent {
 	private sanitizer = inject(DomSanitizer);
 	private logService = inject(LogService);
 	private env = inject(ENV);
+	private swUpdate = inject(SwUpdate);
+	private snackBar = inject(MatSnackBar);
 
 	constructor() {
 		this.iconRegistry.addSvgIcon(
@@ -28,5 +33,14 @@ export class AppComponent {
 		this.logService.info('Client time: ' + today.toString() + ' - Timezone offset: ' + today.getTimezoneOffset() + ' minutes');
 		this.logService.info('Client version: ' + this.env.version);
 		this.logService.info('Client user agent: ' + window.navigator?.userAgent);
+
+		if (this.swUpdate.isEnabled) {
+			this.swUpdate.versionUpdates
+				.pipe(filter((event): event is VersionReadyEvent => event.type === 'VERSION_READY'))
+				.subscribe(() => {
+					const ref = this.snackBar.open('A new version is available.', 'Reload', { panelClass: 'snackbar', duration: 0 });
+					ref.onAction().subscribe(() => document.location.reload());
+				});
+		}
 	}
 }
